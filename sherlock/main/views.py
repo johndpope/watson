da4ae36c554d9fa17ecc0276676740c551111498
Michael Dong <yihao@yihao-mbp.local>
2014-01-25 06:57:34 -0800

    working authentication

--- sherlock/main/views.py
+++ sherlock/main/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, render_to_response
+from django.http import HttpResponse
+from dropbox.client import DropboxOAuth2Flow, DropboxClient
+from django.views.generic import TemplateView
+from db import DBConnector
 
-# Create your views here.
+
+
+APP_KEY = 'nice try'
+APP_SECRET = 'not happening'
+
+def get_dropbox_auth_flow(web_app_session):
+    redirect_uri = "http://localhost:8000/auth_finish"
+    return DropboxOAuth2Flow(APP_KEY, APP_SECRET, redirect_uri,
+        web_app_session, "dropbox-auth-csrf-token")
+
+def auth(request):
+    if 'user_id' in request.session:
+        print request.session['user_id']
+        return render_to_response('index.html')
+    return redirect(get_dropbox_auth_flow(request.session).start())
+
+def dropbox_auth_finish(request):
+    try:
+        access_token, user_id, url_state = \
+            get_dropbox_auth_flow(request.session).finish(request.GET)
+        
+        conn = DBConnector()
+        conn.add_user(user_id, access_token)
+        
+        request.session['user_id'] = user_id
+        return render_to_response('index.html') 
+    except DropboxOAuth2Flow.BadRequestException, e:
+        http_status(400)
+    except DropboxOAuth2Flow.BadStateException, e:
+        # Start the auth flow again.
+        redirect_to("/dropbox-auth-start")
+    except DropboxOAuth2Flow.CsrfException, e:
+        http_status(403)
+    except DropboxOAuth2Flow.NotApprovedException, e:
+        flash('Not approved?  Why not?')
+        return redirect_to("/home")
+    except DropboxOAuth2Flow.ProviderException, e:
+        logger.log("Auth error: %s" % (e,))
+        http_status(403)
--- sherlock/sherlock/urls.py
+++ sherlock/sherlock/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,7 +8,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sherlock.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', TemplateView.as_view(template_name="index.html")), 
+    url(r'^$', 'main.views.auth'), 
+    url(r'^auth_finish', 'main.views.dropbox_auth_finish'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.STATIC_URL}),
