6c3fe8285087a3ebe012bae21a109fd532dc81aa
Michael Dong <yihao@yihao-mbp.local>
2014-01-25 04:14:58 -0800

    folders set up for django

--- sherlock/sherlock/settings.py
+++ sherlock/sherlock/settings.py
@@ -79,4 +79,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = BASE_DIR + '/media/'
+TEMPLATE_DIRS = (
+    BASE_DIR + '/templates/'
+)
--- sherlock/sherlock/urls.py
+++ sherlock/sherlock/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,7 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sherlock.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
+    url(r'^$', TemplateView.as_view(template_name="index.html")), 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.STATIC_URL}),
